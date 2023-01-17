9e0dfb9e67ad42fc3ef0c62b9b9957d42a1a6aa1
BhatiKaran <121540702+BhatiKaran@users.noreply.github.com>
2023-01-17 05:59:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+x=int(input('please give me a number:'))
+
+
+for i in range (1, 11):
+  print(x, "X", i, "=", x*i)
+#i = 12
+#while i>0:
+  #print(x, "X", i, "=", x*i)
+  #i -= 1
+
+
+#
+  
+  
+  
+
+
+  
+  
+
+
+
+  
+
+
+  
+  
+  
+
+
