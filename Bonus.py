1fc2993c7d44378844aa9c359c53cdad74c11b5a
BhatiKaran <121540702+BhatiKaran@users.noreply.github.com>
2023-01-17 10:46:52 +0000

    ^&

---
+++ Bonus.py
@@ -0,0 +1,5 @@
+text= input("enter then title")
+lenght=len(text)
+print ("lenght od the title is",lenght)
+
+
--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-x=int(input('please give me a number:'))
+x = int(input('please give me a number:'))
 
+for i in range(1, 11):
+  print(x, "X", i, "=", x * i)
 
-for i in range (1, 11):
-  print(x, "X", i, "=", x*i)
+print('A', 'B', 'C', 'D', 'E', sep='#')
+
+test = (('12', '13', '14'), 22, 223)
+print(type(test))
+
+for k in test:
+  print(k)
+
 #i = 12
 #while i>0:
-  #print(x, "X", i, "=", x*i)
-  #i -= 1
-
+#print(x, "X", i, "=", x*i)
+#i -= 1
 
 #
-  
-  
-  
-
-
-  
-  
-
-
-
-  
-
-
-  
-  
-  
-
-
